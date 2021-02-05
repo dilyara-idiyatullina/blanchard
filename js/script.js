fe723412df8f69bf6f23606ba702a698757a35fe
Идиятуллина Диляра <dilara111@yandex.ru>
2021-02-05 21:24:12 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,452 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+    let slidesPerViewGallery = 6;
+    let slidesPerViewPublications = 3;
+    let pageNumberGallery = 1;
+    let pageNumberPublications = 1;
+
+    // свайпер hero
+    let heroSwiper = new Swiper('.hero__slider', {
+        // Optional parameters
+        loop: true,
+        autoplay: {
+            delay: 3000,
+        },
+        effect: 'fade',
+        fadeEffect: {
+          crossFade: true
+        },
+
+    }); 
+
+    // свайпер gallery
+    let gallerySwiper = new Swiper('.gallery__slider', {
+        // Optional parameters
+        slidesPerColumn: 2,
+        slidesPerGroup: 3,
+        slidesPerView: 3,
+        spaceBetween: 50,
+        // Responsive breakpoints
+        // breakpoints: {
+        //   // when window width is >= 320px
+        //   320: {
+        //     slidesPerView: 3,
+        //     spaceBetween: 10
+        //   },
+          // when window width is >= 480px
+        //   768: {
+        //     slidesPerView: 2,
+        //     spaceBetween: 30
+        //   },
+        //   // when window width is >= 640px
+        //   1024: {
+        //     slidesPerView: 4,
+        //     spaceBetween: 40
+        //   }
+        //},
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.gallery__switch--next',
+            prevEl: '.gallery__switch--prev',
+        },
+        
+        speed: 800,
+
+        keyboard: {
+            enabled: true,
+            onlyInViewport: true,
+        },
+
+        a11y: {
+            prevSlideMessage: 'Предыдущий слайд',
+            nextSlideMessage: 'Следующий слайд',
+            paginationBulletMessage: 'Перейти к слайду {{index}}',
+        },
+
+    });
+
+    let pagesCountGallery = Math.ceil(gallerySwiper.slides.length / slidesPerViewGallery);
+    document.querySelector('.gallery__pages').innerText = pageNumberGallery + " / " + pagesCountGallery;
+    let realIndexGallery = gallerySwiper.realIndex;
+
+    gallerySwiper.on('realIndexChange', function () {
+
+        if (realIndexGallery >= gallerySwiper.realIndex) {
+            pageNumberGallery = pageNumberGallery - 1; 
+        } else {
+            pageNumberGallery = pageNumberGallery + 1;
+        }
+        realIndexGallery = pageNumberGallery;
+        document.querySelector('.gallery__pages').innerText = pageNumberGallery + " / " + pagesCountGallery;
+
+      });
+
+    // свайпер publications
+    let publicationsSwiper = new Swiper('.publications__slider', {
+        // Optional parameters
+        slidesPerGroup: 3,
+        slidesPerView: 3,
+        spaceBetween: 50,
+        // Responsive breakpoints
+        // breakpoints: {
+        //   // when window width is >= 320px
+        //   320: {
+        //     slidesPerView: 3,
+        //     spaceBetween: 10
+        //   },
+          // when window width is >= 480px
+        //   768: {
+        //     slidesPerView: 2,
+        //     spaceBetween: 30
+        //   },
+        //   // when window width is >= 640px
+        //   1024: {
+        //     slidesPerView: 4,
+        //     spaceBetween: 40
+        //   }
+        //},
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.publications__switch--next',
+            prevEl: '.publications__switch--prev',
+        },
+        
+        speed: 800,
+
+        keyboard: {
+            enabled: true,
+            onlyInViewport: true,
+        },
+
+        a11y: {
+            prevSlideMessage: 'Предыдущий слайд',
+            nextSlideMessage: 'Следующий слайд',
+            paginationBulletMessage: 'Перейти к слайду {{index}}',
+        },
+
+    }); 
+
+    let pagesCountPublications = Math.ceil(publicationsSwiper.slides.length / slidesPerViewPublications);
+    document.querySelector('.publications__pages').innerText = pageNumberPublications + " / " + pagesCountPublications;
+    let realIndexPublications = publicationsSwiper.realIndex;
+
+    publicationsSwiper.on('realIndexChange', function () {
+
+        if (realIndexPublications >= publicationsSwiper.realIndex) {
+            pageNumberPublications = pageNumberPublications - 1; 
+        } else {
+            pageNumberPublications = pageNumberPublications + 1;
+        }
+        realIndexPublications = pageNumberPublications;
+        document.querySelector('.publications__pages').innerText = pageNumberPublications + " / " + pagesCountPublications;
+
+      });
+
+    // свайпер projects
+    let partnersSwiper = new Swiper('.partners__slider', {
+        // Optional parameters
+        loop: true,
+        loopFillGroupWithBlank: true,
+
+        slidesOffsetBefore: 0,
+        slidesPerGroup: 3,
+        slidesPerView: 3,
+        spaceBetween: 50,
+        //Responsive breakpoints
+        // breakpoints: {
+        //   100: {
+        //     slidesPerGroup: 1,
+        //     slidesPerView: 1,
+        //     spaceBetween: 10,
+        //   },
+        //   768: {
+        //     slidesPerView: 2,
+        //     spaceBetween: 50
+        //   },
+        //   1200: {
+        //     slidesPerGroup: 3,
+        //     slidesPerView: 3,
+        //     spaceBetween: 30,
+        //   },
+        //   1600: {
+        //     slidesPerGroup: 3,
+        //     slidesPerView: 3,
+        //     spaceBetween: 50,
+        //   },
+        // },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.partners__switch--next',
+            prevEl: '.partners__switch--prev',
+        },
+        
+        speed: 800,
+
+        keyboard: {
+            enabled: true,
+            onlyInViewport: true,
+        },
+
+        a11y: {
+            prevSlideMessage: 'Предыдущий слайд',
+            nextSlideMessage: 'Следующий слайд',
+            paginationBulletMessage: 'Перейти к слайду {{index}}',
+        },
+
+    }); 
+
+    // плавная навигация по элементам главного меню
+    document.querySelectorAll('.nav-link').forEach(function(navLink) {
+        navLink.addEventListener('click', function(event) {
+
+            event.preventDefault();
+
+            let section = document.querySelector(this.getAttribute('href'));
+
+            let offset = section.offsetTop;
+
+            // вариант 1 - не работает в сафари
+            //section.scrollIntoView({behavior: "smooth"});
+
+            // вариант 2 - не работает в сафари
+            // window.scrollTo({
+            //     top: offset,
+            //     behavior: "smooth"
+            // });
+
+            // вариант 3
+            $("html, body").animate({scrollTop: offset}, 800);
+
+            // вариант 4
+            // должен быть реализован на чистом js (использование анимации?)
+        })
+    })
+
+    // gallery
+    // выпадающий список
+    const element = document.querySelector('#selectFilter');
+        
+    const choices = new Choices(element, {
+        searchEnabled: false,
+        itemSelectText: '',
+        shouldSort: false,
+        choices: [
+            { value: 'автор', label: 'Автор' },
+            { value: 'направление', label: 'Направление' },
+            { value: 'техника', label: 'Техника' },
+          ],
+    });
+
+    // contacts - карта
+    // Функция ymaps.ready() будет вызвана, когда
+    // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+    ymaps.ready(init);
+    function init(){
+        // Создание карты.
+        var myMap = new ymaps.Map("map", {
+            // Координаты центра карты.
+            // Порядок по умолчанию: «широта, долгота».
+            // Чтобы не определять координаты центра карты вручную,
+            // воспользуйтесь инструментом Определение координат.
+            center: [55.75720204375996,37.64243749999998],
+
+            // Уровень масштабирования. Допустимые значения:
+            // от 0 (весь мир) до 19.
+            zoom: 14,
+        });
+                    
+        var myPlacemark = new ymaps.Placemark([55.75846306898368,37.601079499999905], {}, {
+            iconLayout: 'default#image',
+            iconImageHref: '/img/marker.svg',
+            iconImageSize: [20, 20],
+            iconImageOffset: [-3, -42],
+        });
+        
+        // Размещение геообъекта на карте.
+
+        myMap.geoObjects.add(myPlacemark);
+
+    };
+
+    // catalog 
+
+    let painters = [ 
+            { id: 1, country:'italy', century: 15, name: 'Бенедетто ди Биндо', dates: '1401 - 1466.', portret: '', descr: 'dsdcds'}, 
+            { id: 2, country:'italy', century: 15, name: 'Бергоньоне, Амброджо', dates: '', portret: '', descr: 'dsdcds'},
+            { id: 3, country:'italy', century: 15, name: 'Биссоло, Франческо', dates: '', portret: '', descr: 'dsdcds'}, 
+            { id: 4, country:'italy', century: 15, name: 'Больтраффио, Джованни', dates: '', portret: '', descr: 'dsdcds'}, 
+            { id: 5, country:'italy', century: 15, name: 'Бонсиньори, Франческо', dates: '', portret: '', descr: 'dsdcds'}, 
+            { id: 6, country:'italy', century: 15, name: 'Боттичини, Рафаэлло', dates: '', portret: '', descr: 'dsdcds'}, 
+            { id: 7, country:'italy', century: 15, name: 'Брамантино', dates: '', portret: '', descr: 'dsdcds'}, 
+            { id: 8, country:'italy', century: 15, name: 'Бреа, Людовико', dates: '', portret: '', descr: 'dsdcds'}, 
+            { id: 9, country:'italy', century: 15, name: 'Бьяджо д’Антонио Туччи', dates: '', portret: '', descr: 'dsdcds'}, 
+            { id: 10, country:'italy', century: 15, name: 'Веккьетта', dates: '', portret: '', descr: 'dsdcds'}, 
+            { id: 11, country:'italy', century: 15, name: 'Андреа Верроккьо', dates: '', portret: '', descr: 'dsdcds'}, 
+            { id: 12, country:'italy', century: 15, name: 'Доменико Гирландайо', dates: '2 июня 1448 - 11 января 1494.', portret: '/img/domeniko.jpg', descr: 'Один из ведущих флорентийских художников Кватроченто, основатель художественной династии, которую продолжили его брат Давид и сын Ридольфо. Глава художественной мастерской, где юный Микеланджело в течение года овладевал профессиональными навыками. Автор фресковых циклов, в которых выпукло, со всевозможными подробностями показана домашняя жизнь библейских персонажей (в их роли выступают знатные граждане Флоренции в костюмах того времени).'},
+            { id: 13, country:'italy', century: 15, name: 'Беноццо Гоццоли', dates: '', portret: '', descr: 'dsdcds'}, 
+            { id: 14, country:'italy', century: 15, name: 'Граначчи, Франческо', dates: '', portret: '', descr: 'dsdcds'}, 
+            { id: 15, country:'italy', century: 15, name: 'Грегорио ди Чекко', dates: '', portret: '', descr: 'dsdcds'}, 
+            { id: 16, country:'italy', century: 15, name: 'Джованни да Удине', dates: '', portret: '', descr: 'dsdcds'}, 
+            { id: 17, country:'italy', century: 15, name: 'Джованни ди Паоло', dates: '', portret: '', descr: 'dsdcds'}, 
+            { id: 18, country:'italy', century: 15, name: 'Джорджоне', dates: '', portret: '', descr: 'dsdcds'}, 
+            { id: 19, country:'italy', century: 15, name: 'Парентино, Бернардо', dates: '', portret: '', descr: 'dsdcds'}, 
+            { id: 20, country:'italy', century: 15, name: 'Пезеллино', dates: '', portret: '', descr: 'dsdcds'}, 
+            { id: 21, country:'italy', century: 15, name: 'Пьетро Перуджино', dates: '', portret: '', descr: 'dsdcds'}, 
+            { id: 22, country:'italy', century: 15, name: 'Перуцци, Бальдассаре', dates: '', portret: '', descr: 'dsdcds'}, 
+            { id: 23, country:'italy', century: 15, name: 'Пизанелло', dates: '', portret: '', descr: 'dsdcds'}, 
+            { id: 24, country:'italy', century: 15, name: 'Пинтуриккьо', dates: '', portret: '', descr: 'dsdcds'}, 
+            { id: 1, country:'russia', century: 15, name: 'Андрей Рублёв', dates: 'около 1360 — 17 октября 1428.', portret: '/img/rublev.jpg', descr: 'Русский иконописец московской школы иконописи, книжной и монументальной живописи XV века.'}, 
+    ];
+
+    let activeCountry = 'italy';
+    let activePainterNum = 12;
+    let activePainters = painters.filter(item => item.country === activeCountry);
+
+    showPaintersList(activeCountry, activePainterNum);
+    showPainterInfo(activePainterNum);    
+
+    function showPaintersList(countryName, painterNum) {
+
+        activePainters = painters.filter(item => item.country === countryName);
+
+        // цикл по аккордеону с веками    
+        if (activePainters.length > 0) {
+
+            document.querySelectorAll('.dates__list').forEach(function(nodeDatesList) {
+
+                let activePaintersCentury = activePainters.filter(item => item.century == nodeDatesList.dataset.century);
+                if (activePaintersCentury.length < 9) {
+                    nodeDatesList.style.maxHeight = '110px';   
+                } else {
+                    nodeDatesList.style.maxHeight = '343px';                        
+                }
+                
+                let i = 1;
+                let div = document.createElement('div');
+                div.className = "dates__list-column";
+
+                for (let painter of activePaintersCentury) {
+
+                    if (i == painterNum) {
+                        div.innerHTML = div.innerHTML + '<li class="dates__list-item"><a class="dates__link  dates__link--active" data-pid="' + painter.id + '" href="#">' + painter.name + '</a></li>';
+                    } else {
+                        div.innerHTML = div.innerHTML + '<li class="dates__list-item"><a class="dates__link" data-pid="' + painter.id + '" href="#">' + painter.name + '</a></li>';
+                    }
+
+                    if (i % 9 == 0) {
+                        nodeDatesList.append(div);
+                        div = document.createElement('div');
+                        div.className = "dates__list-column";
+                    }
+
+                    i++;
+                }
+
+                nodeDatesList.append(div);
+                
+            });     
+        } 
+    }
+
+    document.querySelectorAll('.flags-list__link').forEach(function(tabsFlag) {
+        tabsFlag.addEventListener('click', function(event) {
+
+            event.preventDefault();
+
+            // очистка дивов 
+            document.querySelectorAll('.dates__list-column').forEach(function(listColumn) {
+                listColumn.remove();
+            });
+
+            // заполнение новых дивов 
+            activeCountry = event.currentTarget.dataset.path;
+            activePainterNum = 1;
+            if (activeCountry == 'italy') activePainterNum = 12;
+
+            showPaintersList(activeCountry, activePainterNum);
+            showPainterInfo(activePainterNum);
+             
+        })
+    })
+
+    document.querySelector('.dates').onclick = function(event) {
+
+        let target = event.target; // где был клик?
+
+        if (target.tagName != 'A') return;
+            
+        event.preventDefault();
+
+        showPainterInfo(target.dataset.pid);
+
+    }
+
+    function showPainterInfo(painterId) {
+
+        let activePainter = activePainters.find(item => item.id == painterId);
+
+        if (activePainter != undefined) {
+
+            document.querySelector('.dates__link--active').classList.remove('dates__link--active');        
+            document.querySelector(`[data-pid="${painterId}"]`).classList.add('dates__link--active');
+
+            document.querySelector('.painter-info__name').innerText = activePainter.name;
+            document.querySelector('.painter-info__dates').innerText = activePainter.dates;
+            document.querySelector('.painter-info__description').innerText = activePainter.descr;
+            document.querySelector('.painter-info__portrait').src = activePainter.portret;
+            document.querySelector('.painter-info__portrait').alt = activePainter.name;
+        }
+        else {
+            document.querySelector('.painter-info__name').innerText = '';
+            document.querySelector('.painter-info__dates').innerText = '';
+            document.querySelector('.painter-info__description').innerText = '';
+            document.querySelector('.painter-info__portrait').src = '#';
+            document.querySelector('.painter-info__portrait').alt = 'нет изображения';            
+        }
+
+    }
+
+    // catalog, accordion on jquery ++++++++++++++++++++++++++++++++++++++++++ 
+
+    $( function() {
+        $( "#accordion" ).accordion();
+      } );
+
+    $( "#accordion" ).accordion({
+        active: false,
+        collapsible: true,
+        heightStyle: "content",
+    });
+
+    document.querySelector('.ui-accordion-header').style.borderBottom = 'none';
+    document.querySelector('.arrow').style.transform = 'rotate(180deg)';
+
+    $( "#accordion" ).on( "accordionbeforeactivate", function( event, ui ) {
+    
+        ui.newHeader.css( "border-bottom", "none" );;
+
+    } );
+  
+    $( "#accordion" ).on( "accordionactivate", function( event, ui ) {
+
+        ui.oldHeader.css( "border-bottom", "1px solid #CACACA" );
+
+        ui.newHeader.find(".arrow").css("transform", "rotate(180deg)");
+        ui.oldHeader.find(".arrow").css("transform", "rotate(360deg)");
+
+    } );
+
+    // events
+
+    document.querySelector('.events__btn').onclick = function(event) {
+        document.querySelector('.events__btn').style.display = "none";
+        document.querySelectorAll('.display-none').forEach(function(event) {        
+            event.classList.remove('display-none');
+        })
+    }
+
+})
+
